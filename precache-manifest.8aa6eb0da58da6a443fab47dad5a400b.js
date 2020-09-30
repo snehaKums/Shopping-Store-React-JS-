751cc958c3dddfcb20758dccca584ffb263bc724
Kumari <sneha.kumari@capgemini.com>
2020-09-30 12:43:03 +0530

    Updates

---
+++ precache-manifest.8aa6eb0da58da6a443fab47dad5a400b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238582e5d53b4ad0326b1dd9a92d8094",
+    "url": "/Shopping-Store-React-JS-/index.html"
+  },
+  {
+    "revision": "621737dbe902affd0146",
+    "url": "/Shopping-Store-React-JS-/static/css/main.f59648f5.chunk.css"
+  },
+  {
+    "revision": "85913fad34fd21c524af",
+    "url": "/Shopping-Store-React-JS-/static/js/2.7b50afdc.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Shopping-Store-React-JS-/static/js/2.7b50afdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621737dbe902affd0146",
+    "url": "/Shopping-Store-React-JS-/static/js/main.61681f16.chunk.js"
+  },
+  {
+    "revision": "67c6dd378715655a666b",
+    "url": "/Shopping-Store-React-JS-/static/js/runtime-main.38b90518.js"
+  }
+]);
